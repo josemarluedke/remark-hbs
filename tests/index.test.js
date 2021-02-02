a434c7b9a3b804d682ca620cf9227ccdad8c9fe4
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-02-02 12:45:24 -0500

    add failing test

--- tests/index.test.js
+++ tests/index.test.js
@@ -6,9 +6,13 @@ const HBS = require('../index');
 
 const stack = unified().use(markdown).use(HBS).use(html);
 
-test('it should keep all the HBS and HTML', () => {
-  const file = stack.processSync(
-    fs.readFileSync(__dirname + '/fixtures/example.md')
-  );
-  expect(file.toString()).toMatchSnapshot();
-});
+let files = [`/fixtures/example.md`, '/fixtures/nested-namespaced.md'];
+
+for (let filePath of files) {
+  test(`${filePath}: it should keep all the HBS and HTML`, () => {
+    const file = stack.processSync(
+      fs.readFileSync(__dirname + filePath)
+    );
+    expect(file.toString()).toMatchSnapshot();
+  });
+}
