82fae38720316b9f1838c078d1d931d853962e6c
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-02-02 13:03:29 -0500

    add validation

--- tests/index.test.js
+++ tests/index.test.js
@@ -3,6 +3,7 @@ const unified = require('unified');
 const markdown = require('remark-parse');
 const html = require('remark-html');
 const HBS = require('../index');
+const { parse } = require('ember-template-recast');
 
 const stack = unified().use(markdown).use(HBS).use(html);
 
@@ -10,9 +11,14 @@ let files = [`/fixtures/example.md`, '/fixtures/nested-namespaced.md'];
 
 for (let filePath of files) {
   test(`${filePath}: it should keep all the HBS and HTML`, () => {
-    const file = stack.processSync(
-      fs.readFileSync(__dirname + filePath)
-    );
-    expect(file.toString()).toMatchSnapshot();
+    const file = stack.processSync(fs.readFileSync(__dirname + filePath));
+
+    const contents = file.toString();
+
+    expect(contents).toMatchSnapshot();
+
+    // Validate that the contents are parseable
+    // this will throw an exception if they are not
+    parse(contents);
   });
 }
