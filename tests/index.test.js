68d5ee8cbd2894098063b98bdddb37c12464c5b7
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-02-02 16:57:06 -0500

    Add specific test for the situation being tested

--- tests/index.test.js
+++ tests/index.test.js
@@ -5,10 +5,12 @@ const html = require('remark-html');
 const HBS = require('../index');
 const { parse } = require('ember-template-recast');
 const chalk = require('chalk');
-
-const emphasize = require('emphasize')
+const emphasize = require('emphasize');
+const { stripIndent } = require('common-tags');
 
 const stack = unified().use(markdown).use(HBS).use(html);
+
+const process = code => stack.processSync(code).toString();
 
 let files = [`/fixtures/example.md`, '/fixtures/nested-namespaced.md'];
 
@@ -20,21 +22,41 @@ for (let filePath of files) {
 
     expect(contents).toMatchSnapshot();
 
-    // Validate that the contents are parseable
-    // this will throw an exception if they are not
-    try {
-      parse(contents);
-    } catch (e) {
-      console.log(
-        chalk.red(e.message) +
-          '\n\n' +
-        emphasize.highlight('handlebars', contents).value +
-          chalk.red(
-            '\n' +
-            `^ ----- contents of ${filePath} after [ markdown -> HBS -> html ] ----- ^`
-          )
-      );
-      throw e;
-    }
+    verifyContents(contents);
   });
+}
+
+test('can render text in a block component', () => {
+  const contents = process(stripIndent`
+    <Nested::Component>
+      some text
+    </Nested::Component>
+  `);
+
+  expect(contents).toEqual(stripIndent`
+    <Nested::Component>
+    some text
+    </Nested::Component>
+  `);
+
+  verifyContents(contents);
+});
+
+// Validate that the contents are parseable
+// this will throw an exception if they are not
+function verifyContents(code) {
+  try {
+    parse(contents);
+  } catch (e) {
+    console.log(
+      chalk.red(e.message) +
+        '\n\n' +
+        emphasize.highlight('handlebars', code).value +
+        chalk.red(
+          '\n' +
+            `^ ----- contents of ${filePath} after [ markdown -> HBS -> html ] ----- ^`
+        )
+    );
+    throw e;
+  }
 }
