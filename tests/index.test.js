4165b1bc7b985690e6b428a3a5d13d94a334b174
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-03-01 16:29:49 -0500

    Clean up other tests with new ignore helpers

--- tests/index.test.js
+++ tests/index.test.js
@@ -3,36 +3,26 @@ const { assertTransform } = require('./utils');
 
 test('can render text in a block component', () => {
   assertTransform({
+    ignoreIndentationChanges: true,
+    ignoreLineBreakChanges: true,
     input: stripIndent`
       <Nested::Component>
         some text
       </Nested::Component>
     `,
-    expected: stripIndent`
-      <Nested::Component>
-
-      some text
-
-      </Nested::Component>
-    `
   });
 });
 
 test('Nested components with nested components', () => {
   assertTransform({
+    ignoreIndentationChanges: true,
+    ignoreLineBreakChanges: true,
     input: stripIndent`
       <Component::With::Nesting>
         <Nested::Component @foo="foo" @bar="bar" />
         <Nested::Component @foo="foo" @bar="bar" />
       </Component::With::Nesting>
     `,
-    expected: stripIndent`
-      <Component::With::Nesting>
-
-      <Nested::Component @foo="foo" @bar="bar" />
-      <Nested::Component @foo="foo" @bar="bar" />
-      </Component::With::Nesting>
-    `
   });
 });
 
